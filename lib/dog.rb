fa4d898816d01f37c52758f45c6996ed2b06575c
Kelly Cotter <kjc853@gmail.com>
2018-07-13 18:52:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,11 @@
 # dog.rb
 
 class Dog
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
-  
+  attr_accessor :name , :breed
+
+
 end
 
+fido = dog.new
+fido.name = "Fido"
+puts fido.breed = "Husky"
